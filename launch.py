9bef95b381253b6ed88fe87ea73636cd53eebd88
c-w-o <77745374+c-w-o@users.noreply.github.com>
2024-07-11 23:40:17 +0200

    Update launch.py

--- launch.py
+++ launch.py
@@ -189,7 +189,7 @@ def filter_preset_mods(local_mods, preset_file=None, cfg_list=None, type="mods")
         for moditem in cfg_list:
             dispname = moditem[0]
             steamid = moditem[1]
-            link_it(type+"/" + steamid, type+"/@" + dispname)
+            link_it(type+"/" + steamid, "@" + dispname)
             moddirs.append( type+"/@" + dispname)
     return moddirs
 
